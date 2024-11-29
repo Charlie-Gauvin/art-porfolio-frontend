b53718cc7bf80efacff5ba2a179aef6b34e55c30
JulienMedina <julien.medina16@gmail.com>
2024-11-29 13:23:26 +0100

    fix: erreur

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,9 +1,65 @@
+"use client";
+import { useState } from "react";
+
+interface FormData {
+  name: string;
+  email: string;
+  sujet: string;
+  message: string;
+}
+
 export default function ContactForm() {
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    sujet: "",
+    message: "",
+  });
+
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/contact`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (res.ok) {
+        setIsSubmitted(true);
+        setFormData({ name: "", email: "", sujet: "", message: "" });
+        alert("Votre message a été envoyé avec succès !");
+      } else {
+        console.error("Erreur lors de l'envoi du message.");
+        alert("Une erreur est survenue, veuillez réessayer.");
+      }
+    } catch (error) {
+      console.error("Erreur lors de l'envoi du message :", error);
+      alert("Une erreur est survenue, veuillez réessayer.");
+    }
+  };
+
   return (
     <section className="h-screen p-6 dark:text-gray-800">
       <form
         noValidate
         className="container mx-auto w-full max-w-xl space-y-6 rounded-md p-8"
+        onSubmit={handleSubmit}
       >
         <div>
           <label htmlFor="name" className="mb-1 ml-1 block">
@@ -14,6 +70,8 @@ export default function ContactForm() {
             type="text"
             placeholder="Nom - Prénom"
             required
+            value={formData.name}
+            onChange={handleChange}
             className="block w-full rounded bg-background2 p-2"
           />
         </div>
@@ -26,19 +84,23 @@ export default function ContactForm() {
             type="email"
             placeholder="Email"
             required
-            className="block w-full rounded bg-background2   p-2"
+            value={formData.email}
+            onChange={handleChange}
+            className="block w-full rounded bg-background2 p-2"
           />
         </div>
         <div>
-          <label htmlFor="email" className="mb-1 ml-1 block">
+          <label htmlFor="sujet" className="mb-1 ml-1 block">
             Sujet
           </label>
           <input
             id="sujet"
-            type="sujet"
-            placeholder="sujet"
+            type="text"
+            placeholder="Sujet"
             required
-            className="block w-full rounded bg-background2  p-2 "
+            value={formData.sujet}
+            onChange={handleChange}
+            className="block w-full rounded bg-background2 p-2"
           />
         </div>
         <div>
@@ -48,15 +110,18 @@ export default function ContactForm() {
           <textarea
             id="message"
             placeholder="Message..."
-            className=" block w-full rounded bg-background2  p-2 "
+            required
+            value={formData.message}
+            onChange={handleChange}
+            className="block w-full rounded bg-background2 p-2"
           ></textarea>
         </div>
         <div>
           <button
             type="submit"
-            className="w-full rounded bg-text3 px-4 py-2 font-bold text-text1  hover:bg-background2 hover:text-text3"
+            className="w-full rounded bg-text3 px-4 py-2 font-bold text-text1 hover:bg-background2 hover:text-text3"
           >
-            Send
+            Envoyer
           </button>
         </div>
       </form>
